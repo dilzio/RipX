7b011f3b7d5267699efe60375288d74738cd02c2
Matt <dilzio@serenity.local>
2013-10-06 15:52:49 +0800

    added application params class

---
+++ src/main/java/org/dilzio/riphttp/util/ApplicationParams.java
@@ -0,0 +1,40 @@
+package org.dilzio.riphttp.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationParams {
+	private Map<ParamEnum, String> _paramMap = new HashMap<ParamEnum, String>();
+
+	public int getIntParam(ParamEnum paramName) {
+		if (_paramMap.containsKey(paramName)){
+			return Integer.valueOf(_paramMap.get(paramName)).intValue();
+		}
+			return Integer.valueOf(paramName.getDefault()).intValue();
+	}
+
+	public boolean getBoolParam(ParamEnum paramName) {
+		if (_paramMap.containsKey(paramName)){
+			return Boolean.valueOf(_paramMap.get(paramName)).booleanValue();
+		}
+			return Boolean.valueOf(paramName.getDefault()).booleanValue();
+	}
+
+	public float getFloatParam(ParamEnum paramName) {
+		if (_paramMap.containsKey(paramName)){
+			return Float.valueOf(_paramMap.get(paramName)).floatValue();
+		}
+			return Float.valueOf(paramName.getDefault()).floatValue();
+	}
+
+	public String getStringParam(ParamEnum paramName) {
+		if (_paramMap.containsKey(paramName)){
+			return _paramMap.get(paramName);
+		}
+			return paramName.getDefault(); 
+	}
+
+	public void setParam(ParamEnum paramName, String paramVal) {
+		_paramMap.put(paramName, paramVal);
+	}
+}
---
+++ src/main/java/org/dilzio/riphttp/util/ParamEnum.java
@@ -0,0 +1,21 @@
+package org.dilzio.riphttp.util;
+
+public enum ParamEnum {
+	LISTEN_PORT("8081"),
+	WORKER_COUNT("1"),
+	RING_BUFFER_SIZE("8192"),
+	USE_SSL("false"),
+	SSL_KEYSTORE(null),
+	SSL_KEYSTORE_PASSWORD(null),
+	TEST_FLOAT("1.35");
+	
+	private String _default;
+	
+	ParamEnum(String defaultVal){
+		_default = defaultVal;
+	}
+	
+	public String getDefault(){
+		return _default;
+	}
+}
